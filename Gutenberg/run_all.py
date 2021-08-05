66f30abe65ae0ae76b41fff46a1ad864db5d95b9
alonkipnis <alonkipnis@gmail.com>
2021-08-05 15:20:34 +0300

    submodule

--- Gutenberg/run_all.py
+++ Gutenberg/run_all.py
@@ -27,56 +27,56 @@ clf_names=[
 
 
 def main() :
-  parser = argparse.ArgumentParser()
-  parser = argparse.ArgumentParser(description='Evaluate hardcoded list of '
-  'classifiers Authorship challenge')
-  parser.add_argument('-i', type=str, help='data file (csv)')
-  parser.add_argument('-n', type=int, help='n split (integer)', default=20)
-  args = parser.parse_args()
-  if not args.i:
-      print('ERROR: The data file is required')
-      parser.exit(1)
-  else :
-    data_path = args.i
+	parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser(description='Evaluate hardcoded list of '
+	'classifiers Authorship challenge')
+	parser.add_argument('-i', type=str, help='data file (csv)')
+	parser.add_argument('-n', type=int, help='n split (integer)', default=20)
+	args = parser.parse_args()
+	if not args.i:
+			print('ERROR: The data file is required')
+			parser.exit(1)
+	else :
+		data_path = args.i
 
-  print('\tData: {}'.format(data_path))
-  print('\tNumber of train/val splits = {}'.format(args.n))
+	print('\tData: {}'.format(data_path))
+	print('\tNumber of train/val splits = {}'.format(args.n))
 
-  vocab_sizes=[250, 1000, 3000]
-  
+	vocab_sizes=[250, 1000, 3000]
+	
 
-  n_split = args.n
-  no_vocab_sizes = len(vocab_sizes)
-  no_clf = len(clf_names)
+	n_split = args.n
+	no_vocab_sizes = len(vocab_sizes)
+	no_clf = len(clf_names)
 
-  df = pd.DataFrame()
-  for ic in range(no_clf) :
-    for iv in range(no_vocab_sizes) :
-        vocab_size = vocab_sizes[iv]
-        clf_name = clf_names[ic]
-        print("classifier = {}".format(clf_name))
-	fn = args.i.split('.pkl')[0]
-	data_file = fn + f'_{vocab_size}.pkl'
+	df = pd.DataFrame()
+	for ic in range(no_clf) :
+		for iv in range(no_vocab_sizes) :
+				vocab_size = vocab_sizes[iv]
+				clf_name = clf_names[ic]
+				print("classifier = {}".format(clf_name))
+				fn = args.i.split('.pkl')[0]
+				data_file = fn + f'_{vocab_size}.pkl'
 
-        print(f"Loading data from {ata_file}..,", end=' ')
-	try :
-        	data = get_word_counts_from_pkl_file(data_file)
-	except : 
-		print("Could not load data file")
-		continue
-        print(f"Found {len(data[0])} samples.")
+				print(f"Loading data from {ata_file}..,", end=' ')
+				try :
+					data = get_word_counts_from_pkl_file(data_file)
+				except : 
+					print("Could not load data file")
+					continue
+				print(f"Found {len(data[0])} samples.")
 
-        acc, std = evaluate_classifier(clf_name, data, n_split)
-        print("Average accuracy = {}".format(acc))  
-        print("STD = {}".format(std))
+				acc, std = evaluate_classifier(clf_name, data, n_split)
+				print("Average accuracy = {}".format(acc))  
+				print("STD = {}".format(std))
 
-        df = df.append({'clf_name' : clf_name,
-            'dataset' : data_file,
-            'accuracy' : acc,
-            'std' : std,
-            'n_split' : n_split
-            }, ignore_index = True)        
-        df.to_csv('results.csv')
+				df = df.append({'clf_name' : clf_name,
+						'dataset' : data_file,
+						'accuracy' : acc,
+						'std' : std,
+						'n_split' : n_split
+						}, ignore_index = True)        
+				df.to_csv('results.csv')
 
 if __name__ == '__main__':
-  main()
+	main()
